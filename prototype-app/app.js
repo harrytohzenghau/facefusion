e4413bf83dba2d2a3d634c6419e0655b44e44b56
MonokumeType01 <69583268+MonokumeType01@users.noreply.github.com>
2024-09-05 21:54:28 +0800

    update: navbar and html pages added

--- prototype-app/app.js
+++ prototype-app/app.js
@@ -1,24 +1,15 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
-app.use(express.static('public'));
 
-// routes
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.get('/about', (req, res) => {
-    res.send('This is the about page.');
-  });
-  
-  app.get('/contact', (req, res) => {
-    res.send('This is the contact page.');
-  });
-  
-
-// Start the server
 app.listen(port, () => {
   console.log(`App is running at http://localhost:${port}`);
 });
