de9ad897f308d16707b06c48b817291f32c7640e
MonokumeType01 <wongshengxiang@gmail.com>
2024-10-10 21:57:33 +0800

    Added Seed for user role

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
 require('dotenv').config();
+const UserRole = require('./models/UserRole'); // Import the UserRole model
 
 const app = express();
 
@@ -9,6 +10,31 @@ connectDB();
 
 // Middleware to parse JSON
 app.use(express.json());
+
+// Seeds(This one to add all the roles on startup)
+const seedRoles = async () => {
+  const roles = [
+    { id: 1, name: 'Admin' },
+    { id: 2, name: 'Guest' },
+    { id: 3, name: 'Free' },
+    { id: 4, name: 'Premium' }
+  ];
+
+  try {
+    const existingRoles = await UserRole.find();
+    if (existingRoles.length === 0) {
+      await UserRole.insertMany(roles);
+      console.log('Roles seeded successfully');
+    } else {
+      console.log('Roles already exist, skipping seeding');
+    }
+  } catch (error) {
+    console.error('Error seeding roles:', error);
+  }
+};
+
+// Call the seedRoles function after connecting to the database
+seedRoles();
 
 // Define Routes
 app.use('/api/auth', require('./routes/authRoutes')); // Auth routes for login and registration
@@ -20,8 +46,6 @@ app.use('/api/ratings', require('./routes/ratingsRoutes')); // Route for handlin
 app.use('/api/users', require('./routes/users'));
 app.use('/api/userRoles', require('./routes/userRoles'));
 app.use('/api/contentBank', require('./routes/contentBank'));
-
-
 
 // Start the server
 const PORT = process.env.PORT || 5000;
