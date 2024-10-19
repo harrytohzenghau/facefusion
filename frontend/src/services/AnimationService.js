69fdcfd1fb2ae177a7eff5f069da4e71ce444f19
MonokumeType01 <wongshengxiang@gmail.com>
2024-10-20 03:30:49 +0800

    updated generateLipSync() for frontend

--- frontend/src/services/AnimationService.js
+++ frontend/src/services/AnimationService.js
@@ -46,5 +46,44 @@ export const generateExpression = async (image, expression) => {
 };
 
 export const generateLipSync = async (face, audio) => {
+  const token = store.getState().auth.token;
+  const formData = new FormData();
 
+  try {
+    if (!face || !audio) {
+      console.error("Face video or audio is missing.");
+      return { success: false, message: "Invalid input." };
+    }
+
+    formData.append("face", face); // ensure face is a valid File object
+    formData.append("audio", audio); // append the audio file
+
+    const response = await axios.post(
+      `${apiUrl}/api/animation/lipSync`,
+      formData,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    return { success: true, data: response.data };
+  } catch (error) {
+    if (error.response) {
+      console.log(error);
+      return {
+        success: false,
+        message:
+          error.response.data.error || "An error occurred on the server.",
+      };
+    } else if (error.request) {
+      return {
+        success: false,
+        message: "No response from the server. Please try again later.",
+      };
+    } else {
+      return { success: false, message: "An unexpected error occurred." };
+    }
+  }
 };
