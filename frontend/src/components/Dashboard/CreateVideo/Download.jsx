1ba86548f21bbd3bfe37dc1532770be3bd7a65f0
harrytohzenghau <zenghautoh@gmail.com>
2024-10-25 23:35:39 +0800

    Added audio input

--- frontend/src/components/Dashboard/CreateVideo/Download.jsx
+++ frontend/src/components/Dashboard/CreateVideo/Download.jsx
@@ -1,18 +1,23 @@
 const Download = ({ video }) => {
+  const handleDownload = () => {
+    const link = document.createElement("a");
+    link.href = video;
+    link.download = "video.mp4"; // Set a default file name for download
+    link.click();
+  };
+
   return (
     <div className="flex flex-col gap-y-6">
       <div>
         <video className="w-3/5 my-0 mx-auto" controls>
-          <source
-            src={video}
-            type="video/mp4"
-          />
+          <source src={video} type="video/mp4" />
           Your browser does not support the video tag.
         </video>
       </div>
       <button
         className="bg-blue-1 text-white py-2 rounded-lg hover:bg-blue-2 transform transition-all duration-200 ease-in-out"
-        type="submit"
+        type="button"
+        onClick={handleDownload}
       >
         Download
       </button>
