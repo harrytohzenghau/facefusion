9c5153e7147654c94bb01d30aa7ffe654a89de4e
harrytohzenghau <zenghautoh@gmail.com>
2024-11-10 17:57:28 +0800

    Update AdminNavigation.jsx

--- frontend/src/components/Layout/AdminNavigation.jsx
+++ frontend/src/components/Layout/AdminNavigation.jsx
@@ -35,62 +35,6 @@ const AdminNavigation = () => {
   };
 
   return (
-    // <nav className="static top-0 w-full bg-gradient-to-r from-blue-9 to-blue-5 px-6 py-2">
-    //   <div className="flex justify-between items-center w-4/5 min-w-[680px] mx-auto my-0">
-    //     <div className="w-14">
-    //       <img src={logo} alt="FaceFusion" />
-    //     </div>
-    //     <ul className="flex gap-x-8">
-    //       <li>
-    //         <NavLink
-    //           to="/admin"
-    //           end
-    //           className={({ isActive }) => {
-    //             return isActive
-    //               ? "text-blue-3 font-bold hover:text-blue-3 transform transition-all duration-200 ease-in-out"
-    //               : "text-blue-2 font-bold hover:text-blue-3 transform transition-all duration-200 ease-in-out";
-    //           }}
-    //         >
-    //           User
-    //         </NavLink>
-    //       </li>
-    //       <li>
-    //         <NavLink
-    //           to="/admin/admin-list"
-    //           className={({ isActive }) => {
-    //             return isActive
-    //               ? "text-blue-3 font-bold hover:text-blue-3 transform transition-all duration-200 ease-in-out"
-    //               : "text-blue-2 font-bold hover:text-blue-3 transform transition-all duration-200 ease-in-out";
-    //           }}
-    //         >
-    //           Admin
-    //         </NavLink>
-    //       </li>
-    //       <li>
-    //         <NavLink
-    //           to="/admin/rating"
-    //           className={({ isActive }) => {
-    //             return isActive
-    //               ? "text-blue-3 font-bold hover:text-blue-3 transform transition-all duration-200 ease-in-out"
-    //               : "text-blue-2 font-bold hover:text-blue-3 transform transition-all duration-200 ease-in-out";
-    //           }}
-    //         >
-    //           Rating
-    //         </NavLink>
-    //       </li>
-    //     </ul>
-    //     <ul className="flex flex-end gap-x-8">
-    //       <li>
-    //         <button
-    //           onClick={logoutHandler}
-    //           className="text-blue-2 font-bold hover:text-blue-3 transform transition-all duration-200 ease-in-out"
-    //         >
-    //           Logout
-    //         </button>
-    //       </li>
-    //     </ul>
-    //   </div>
-    // </nav>
     <nav className="flex items-center justify-between flex-wrap bg-gradient-to-r from-blue-9 to-blue-5 px-6 py-2">
       <div className="flex justify-between items-center w-4/5 mx-auto my-0 max-lg:w-full">
         <div className="flex items-center flex-shrink-0 text-white mr-6 w-14">
@@ -153,6 +97,18 @@ const AdminNavigation = () => {
               </li>
             </ul>
             <ul className="flex flex-end gap-x-8">
+              <li>
+                <NavLink
+                  to="/admin/admin-profle"
+                  className={({ isActive }) => {
+                    return isActive
+                      ? "text-blue-3 font-bold hover:text-blue-3 transform transition-all duration-200 ease-in-out"
+                      : "text-blue-2 font-bold hover:text-blue-3 transform transition-all duration-200 ease-in-out";
+                  }}
+                >
+                  Profile
+                </NavLink>
+              </li>
               <li>
                 <button
                   onClick={logoutHandler}
