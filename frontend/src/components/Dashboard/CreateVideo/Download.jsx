c796a2fb7f827f6360dcd1fffa4c5a9f5901e41d
harrytohzenghau <zenghautoh@gmail.com>
2024-11-10 16:44:22 +0800

    Update Download.jsx

--- frontend/src/components/Dashboard/CreateVideo/Download.jsx
+++ frontend/src/components/Dashboard/CreateVideo/Download.jsx
@@ -29,30 +29,21 @@ const Download = ({ video, contentId }) => {
     }
   };
 
-  const handleDelete = () => {}
-
   return (
-    <div className="flex gap-x-6 items-start">
-      <div className="w-1/5">
+    <div className="flex flex-col gap-x-6 gap-y-6 items-start">
+      <div className="w-3/5 max-w-[300px] mx-auto">
         <video className="w-full my-0 mx-auto" controls>
           <source src={video} type="video/mp4" />
           Your browser does not support the video tag.
         </video>
       </div>
-      <div className="w-2/5 flex flex-col gap-y-4">
+      <div className="w-full flex flex-col gap-y-4">
         <button
-          className="bg-blue-1 w-2/5 text-white py-2 rounded-lg hover:bg-blue-2 transform transition-all duration-200 ease-in-out"
+          className="bg-blue-1 text-white py-2 rounded-lg hover:bg-blue-2 transform transition-all duration-200 ease-in-out"
           type="button"
           onClick={handleDownload}
         >
           Download
-        </button>
-        <button
-          className="bg-red-50 w-2/5 text-white py-2 rounded-lg hover:bg-red-400 transform transition-all duration-200 ease-in-out"
-          type="button"
-          onClick={handleDelete}
-        >
-          Delete
         </button>
       </div>
     </div>
